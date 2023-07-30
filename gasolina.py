2e9cda62805e220ca19b3d3652b66408ed7a20a7
malucor <luiza1772@gmail.com>
2023-07-30 16:04:20 +0000

    arquivo gasolina.py atualizado

--- gasolina.py
+++ gasolina.py
@@ -1,23 +1,10 @@
+
 import pandas as pd
 import seaborn as sns
 
-#%%writefile gasolina.csv
-#dia,venda
-#1,5.11
-#2,4.99
-#3,5.02
-#4,5.21
-#5,5.07
-#6,5.09
-#7,5.13
-#8,5.12
-#9,4.94
-#10,5.03
-
 gasolina_df = pd.read_csv('gasolina.csv', sep=',')
-gasolina_df
 
 with sns.axes_style ('whitegrid'):
   grafico_gasolina = sns.lineplot(data=gasolina_df, x='dia', y='venda')
-  grafico_gasolina.set(title='Gasolina em São Paulo/SP nos 10 primeiros dias de Julho de 2021', xlabel='Dia', ylabel='Preço')
+  grafico_gasolina.set(title='Gasolina em São Paulo/SP nos 10 primeiros dias de Julho/2021', xlabel='Data', ylabel='Venda')
   grafico_gasolina.get_figure().savefig(f"gasolina.png")
